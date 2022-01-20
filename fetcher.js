02e9b2b76f7bb3eb18c1f7fc8905a54d035e72ed
Gerard Compion <gcompion93@gmail.com>
2022-01-19 21:43:53 -0500

    added check for if file to be written already exists, follows up with rl.question y/n to replace

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,50 @@
 const fs = require('fs');
+const readline = require('readline');
 const request = require('request');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const args = process.argv.slice(2);
 
-// expects first argument to be a URL, GETs info from said URL
-request(args[0], (error, response, body) => {
-  // writes HTML content (body) of webpage at given URL to the given path
-  fs.writeFile(args[1], body, err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    // character count = file size in bytes for text files
-    let charCount = 0;
-    // loops through characters in body to calculate filesize in bytes
-    for (const character of body) {
-      ++charCount;
-    }
-    console.log(`Downloaded and saved ${charCount} bytes to ${args[1]}`);
+// most of the logic in a separate function in order to interact with fs.access check neatly
+const readAndWrite = () => {
+  // expects first argument to be a URL, GETs info from said URL
+  request(args[0], (error, response, body) => {
+    // writes HTML content (body) of webpage at given URL to the given path
+    fs.writeFile(args[1], body, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // character count = file size in bytes for text files
+      let charCount = 0;
+      // loops through characters in body to calculate filesize in bytes
+      for (const character of body) {
+        ++charCount;
+      }
+      console.log(`Downloaded and saved ${charCount} bytes to ${args[1]}`);
+    });
   });
+};
+
+fs.access(args[1], fs.F_OK, (err) => {
+  if (err) {
+    readAndWrite();
+    return;
+  } else {
+    rl.question(`${args[1]} already exists. Would you like to replace it? (y/n)  `, (answer => {
+    if (answer === 'y') {
+      readAndWrite();
+      rl.close();
+    } else if (answer === 'n') {
+      rl.close();
+    } else {
+      console.log('Invalid response. Ending program. Please try again.');
+    }
+  }));}
 });
+
 
